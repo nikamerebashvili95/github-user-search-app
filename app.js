cf714273ded672e7696682c158eba2375e1a4801
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-02-21 04:49:58 -0800

    work with form

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const moon = document.querySelector("#moon");
 const sun = document.querySelector("#sun");
 const title = document.querySelector(".title");
 const themeText = document.querySelector(".theme-text");
+const input = document.querySelector("#user");
+const btn = document.querySelector(".btn");
 const flitTheme = (theme) => {
   if (theme === "dark") {
     moon.style.display = "none";
@@ -13,3 +15,6 @@ const flitTheme = (theme) => {
   themeText.classList.toggle("dark");
 };
 moon.addEventListener("click", () => flitTheme("dark"));
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
