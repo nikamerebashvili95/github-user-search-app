54817c1e7c0e779ee13f1ee9bf6abb05020d02b7
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-02-21 05:11:55 -0800

    add axios

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ const title = document.querySelector(".title");
 const themeText = document.querySelector(".theme-text");
 const input = document.querySelector("#user");
 const btn = document.querySelector(".btn");
+const octocad = {
+  avatar_url: "https://avatars.githubusercontent.com/u/583231?v=4",
+  bio: null,
+  blog: "https://github.blog",
+  company: "@github",
+  created_at: "2011-01-25T18:44:36Z",
+  email: null,
+  events_url: "https://api.github.com/users/octocat/events{/privacy}",
+  followers: 8413,
+  followers_url: "https://api.github.com/users/octocat/followers",
+  following: 9,
+  following_url: "https://api.github.com/users/octocat/following{/other_user}",
+  gists_url: "https://api.github.com/users/octocat/gists{/gist_id}",
+  gravatar_id: "",
+  hireable: null,
+  html_url: "https://github.com/octocat",
+  id: 583231,
+  location: "San Francisco",
+  login: "octocat",
+  name: "The Octocat",
+  node_id: "MDQ6VXNlcjU4MzIzMQ==",
+  organizations_url: "https://api.github.com/users/octocat/orgs",
+  public_gists: 8,
+  public_repos: 8,
+  received_events_url: "https://api.github.com/users/octocat/received_events",
+  repos_url: "https://api.github.com/users/octocat/repos",
+  site_admin: false,
+  starred_url: "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/octocat/subscriptions",
+  twitter_username: null,
+  type: "User",
+  updated_at: "2023-01-22T12:13:51Z",
+  url: "https://api.github.com/users/octocat",
+};
 const flitTheme = (theme) => {
   if (theme === "dark") {
     moon.style.display = "none";
@@ -15,6 +49,16 @@ const flitTheme = (theme) => {
   themeText.classList.toggle("dark");
 };
 moon.addEventListener("click", () => flitTheme("dark"));
-btn.addEventListener("click", (e) => {
+btn.addEventListener("click", async (e) => {
   e.preventDefault();
+  try {
+    const response = await axios.get(
+      "https://api.github.com/users/" + input.value
+    );
+    const user = response.data;
+    input.value = "";
+    console.log(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
